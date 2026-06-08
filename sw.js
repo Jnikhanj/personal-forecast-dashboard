8d2abd0c9a0206c73487e9777acd14f823a7b226
Jnikhanj <Nikhanjjashan@gmail.com>
2026-06-08 19:44:58 +1000

    Cache app icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="personal-predictor-v3-1";
-const ASSETS=["./","./index.html","./style.css?v=3.0","./app.js?v=3.1","./manifest.webmanifest"];
+const CACHE_NAME="personal-predictor-v3-2";
+const ASSETS=["./","./index.html","./style.css?v=3.0","./app.js?v=3.1","./manifest.webmanifest","./icon.svg","./apple-touch-icon.png"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));});
